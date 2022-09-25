de3071106d2e9492b59ecd9e65fb572165083f56
SHAIG JAFAROV <shaiq.jafarov@abb-bank.az>
2022-09-25 12:20:54 +0400

    Initial commit

---
+++ src/com/company/lesson2/Main.java
@@ -0,0 +1,47 @@
+package com.company.lesson2;
+
+public class Main {
+
+
+    long l = 3;
+    public static void nese() {
+
+    }
+
+    public static void main(String[] args) {
+        long c = 237623429834l;
+        long a = 222222222237846L;
+
+        {
+            int aqwer=56;
+        }
+        char qq = '2';
+        char ww = '$';
+
+        boolean wwe = true;
+        boolean ads = false;
+
+
+        String str = "";
+
+
+        int www = '3';
+        System.out.println((int) qq + " " + (int) ww);
+        System.out.println(www);
+
+
+        System.out.println(qq + ww);
+
+
+//        double d=  ;
+        double f = 10.0 / 4;
+
+        double yy = 10_000_000_000.0;
+        float rtt = 100.0f;
+//        int v=
+//        System.out.println(10.0/4);
+//        System.out.println(d);
+
+//        System.out.println(23487234);
+    }
+}
---
+++ src/com/company/lesson2/TestClas.java
@@ -0,0 +1,14 @@
+package com.company.lesson2;
+
+public class TestClas {
+    public static void main(String[] args) {
+        int hex=0x123;
+        int binary=0b101;
+        int octa=0123;
+        System.out.println(hex);
+        System.out.println(binary);
+        System.out.println(octa);
+
+
+    }
+}
