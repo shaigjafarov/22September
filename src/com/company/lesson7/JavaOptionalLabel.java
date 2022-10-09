f1d949380f8614eb2a31a53b86f3f0e8a8c3cb8b
SHAIG JAFAROV <shaiq.jafarov@abb-bank.az>
2022-10-09 11:31:46 +0400

    chnage variable

--- src/com/company/lesson7/JavaOptionalLabel.java
+++ src/com/company/lesson7/JavaOptionalLabel.java
@@ -2,13 +2,19 @@ package com.company.lesson7;
 
 public class JavaOptionalLabel {
     public static void main(String[] args) {
+        int as=4;
 
-         OUT: for (int i = 0; i < 10; i++) {
+        as+=(long)5;
+
+//        as =  (long)(as + 5);
+
+        AUE:
+        for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
 
                 System.out.println(i + " " + j);
                 if (j == 5)
-                    break OUT;
+                    break AUE;
 
             }
 
---
+++ src/com/company/lesson8/ChangeValue.java
@@ -0,0 +1,16 @@
+package com.company.lesson8;
+
+public class ChangeValue {
+
+    public static void main(String[] args) {
+        int a=5, b=3;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+
+
+
+    }
+}
